9954a3dcde39f4fd1aba799472317bc8adc654bf
Victoria Goltseva <katnisseverdeen191@mail.ru>
2023-08-24 20:10:51 +0300

    init project

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.getElementById('button').onclick = function () {
+    document.getElementById('footer').scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById('services-link').onclick = function () {
+    document.getElementById('services').scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById('why-link').onclick = function () {
+    document.getElementById('why').scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById('footer-link').onclick = function () {
+    document.getElementById('footer').scrollIntoView({behavior: "smooth"});
+}
+
+document.getElementById('gallery-link').onclick = function () {
+    document.getElementById('gallery').scrollIntoView({behavior: "smooth"});
+}
